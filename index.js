06f3aa952ca0d8c55d788a6afefe072128afe5fd
sarahbaranski <sbaranski@gmail.com>
2022-04-09 11:03:57 -0500

    Display note titles from an array of notes

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 /* global Vue */
 
-console.log("Hello from JavaScript", Vue);
+var App = {
+  data: function () {
+    return {
+      notes: [
+        { id: 1, body: "This is a first test", timestamp: Date.now() - 2000000 },
+        { id: 2, body: "This is a second test", timestamp: Date.now() - 1000000 },
+        { id: 3, body: "This is a third test", timestamp: Date.now() },
+      ],
+    };
+  },
+};
+
+Vue.createApp(App).mount("#app");
