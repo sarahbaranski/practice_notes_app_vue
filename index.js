68962dde0fe3f295ccffd9e32ba00690f67c5bdc
sarahbaranski <sbaranski@gmail.com>
2022-04-09 11:07:40 -0500

    Use methods to sort and format notes

--- index.js
+++ index.js
@@ -7,8 +7,31 @@ var App = {
         { id: 1, body: "This is a first test", timestamp: Date.now() - 2000000 },
         { id: 2, body: "This is a second test", timestamp: Date.now() - 1000000 },
         { id: 3, body: "This is a third test", timestamp: Date.now() },
+        { id: 4, body: "", timestamp: Date.now() + 1000000 },
       ],
     };
+  },
+  computed: {
+    transformedNotes: function () {
+      return this.notes.slice().sort(function (a, b) {
+        return b.timestamp - a.timestamp;
+      });
+    },
+  },
+  methods: {
+    formatTitle: function (body) {
+      var maxLength = 20;
+      if (body.length > maxLength) {
+        return body.substring(0, maxLength - 3) + "...";
+      } else if (body.length === 0) {
+        return "New note";
+      } else {
+        return body;
+      }
+    },
+    formatTimestamp: function (timestamp) {
+      return new Date(timestamp).toUTCString();
+    },
   },
 };
 
